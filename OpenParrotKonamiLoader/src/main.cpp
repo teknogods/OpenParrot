4c9d55350f779c1e7386813afb5cfccd306bed2d
Reaver <reaver@teknogods.com>
2018-09-13 22:11:35 +0300

    Dongle emulated for Konami and some more libio apis.
    Seems to be missing some libavs stuff now, need to debug futher :(

--- OpenParrotKonamiLoader/src/main.cpp
+++ OpenParrotKonamiLoader/src/main.cpp
@@ -1,12 +1,109 @@
 #include <Windows.h>
 #include "libavs-win32.h"
 #include "libacio.h"
+#include <MinHook.h>
 
 typedef signed(__stdcall *dll_entry_main)();
 typedef signed(__stdcall *dll_entry_init)();
 
 #include <stdio.h>
 #include <iostream>
+int __cdecl bt_get_ikey_status(unsigned char *a1)
+{
+	memset(a1, 0, 28);
+	a1[4] = 1;
+	a1[6] = 1;
+	a1[12] = -1;
+	a1[20] = 1;
+	a1[22] = 1;
+	a1[28] = -1;
+	return 0;
+}
+
+int bt_fcheck_main()
+{
+	return 0;
+}
+
+void bt_fcheck_init()
+{
+	// Do nothing
+}
+
+void bt_fcheck_finish()
+{
+	// Do nothing
+}
+
+HMODULE (WINAPI *OrigGetModuleHandleA)(
+	_In_opt_ LPCTSTR lpModuleName
+);
+
+HMODULE WINAPI GetModuleHandleAWrap(
+	_In_opt_ LPCTSTR lpModuleName
+)
+{
+	if ((DWORD)lpModuleName == 0x00)
+	{
+		return OrigGetModuleHandleA(lpModuleName);
+	}
+
+	if(strlen(lpModuleName) != 7)
+	{
+		return OrigGetModuleHandleA(lpModuleName);
+	}
+
+	if(strcmp(lpModuleName, "kbt.dll") == 0)
+	{
+		return (HMODULE)0x8001;
+	}
+
+	if(strcmp(lpModuleName, "kld.dll") == 0)
+	{
+		return (HMODULE)0x8002;
+	}
+
+	return OrigGetModuleHandleA(lpModuleName);
+}
+
+FARPROC(WINAPI *OrigGetProcAddress)(
+	_In_ HMODULE hModule,
+	_In_ LPCSTR  lpProcName
+	);
+
+FARPROC WINAPI GetProcAddressWrap(
+	_In_ HMODULE hModule,
+	_In_ LPCSTR  lpProcName
+)
+{
+	if(hModule == (HMODULE)0x8001)
+	{
+		// kbt.dll
+		if(strcmp(lpProcName, "bt_get_ikey_status") == 0)
+		{
+			return (FARPROC)bt_get_ikey_status;
+		}
+	}
+
+	if(hModule == (HMODULE)0x8002)
+	{
+		// kld.dll
+		if(strcmp(lpProcName, "bt_fcheck_init") == 0)
+		{
+			return (FARPROC)bt_fcheck_init;
+		}
+		if(strcmp(lpProcName, "bt_fcheck_main") == 0)
+		{
+			return (FARPROC)bt_fcheck_main;
+		}
+		if (strcmp(lpProcName, "bt_fcheck_finish") == 0)
+		{
+			return (FARPROC)bt_fcheck_finish;
+		}
+	}
+
+	return OrigGetProcAddress(hModule, lpProcName);
+}
 
 inline bool file_exists(const std::string& name)
 {
@@ -30,14 +127,14 @@ int main(int argc, char* argv[])
 	}
 
 	init_libavs();
-
-	// Add some sleep just in case.
-	Sleep(1000);
-
 	init_libacioHooks();
 
-	// Add some sleep just in case.
-	Sleep(1000);
+
+	// Hook dongle stuff without needing any lame dlls
+	MH_Initialize();
+	MH_CreateHookApi(L"kernel32.dll", "GetModuleHandleA", GetModuleHandleAWrap, (void **)&OrigGetModuleHandleA);
+	MH_CreateHookApi(L"kernel32.dll", "GetProcAddress", GetProcAddressWrap, (void **)&OrigGetProcAddress);
+	MH_EnableHook(MH_ALL_HOOKS);
 
 	auto gameBinary = LoadLibraryA(argv[1]);
 
