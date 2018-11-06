f3fd6dd294d10a5c3011fb576b32660a8588acf7
Boomslangnz <Boomslangnz@hotmail.com>
2018-11-06 23:02:11 +1300

    Fixed x64 not building correctly

--- OpenParrot/src/Functions/PokkenXInputEmu.cpp
+++ OpenParrot/src/Functions/PokkenXInputEmu.cpp
@@ -76,12 +76,12 @@ DWORD WINAPI XInputGetState
 			gamepadState.wButtons |= *ffbOffset;
 		else
 			gamepadState.wButtons |= 0;
-
+#ifndef _M_AMD64
 		if (GameDetect::currentGame == GameID::Daytona3)
 		{
 			gamepadState.bRightTrigger = daytonaPressStart ? 0xFF : 0x00;
 		}
-
+#endif
 			if (pState->dwPacketNumber == UINT_MAX)
 				pState->dwPacketNumber = 0;
 			else
@@ -280,11 +280,13 @@ DWORD WINAPI XInputGetStateEx
 		else
 			gamepadState.wButtons = 0;
 
+#ifndef _M_AMD64
 		if (GameDetect::currentGame == GameID::Daytona3)
 		{
 			gamepadState.bRightTrigger = daytonaPressStart ? 0xFF : 0x00;
 		}
-
+#endif
+		
 		if (pState->dwPacketNumber == UINT_MAX)
 			pState->dwPacketNumber = 0;
 		else
