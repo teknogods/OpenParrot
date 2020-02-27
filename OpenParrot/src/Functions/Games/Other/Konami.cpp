32f47f1e1811294dce5e359541454cf460d14d22
nibs <Poliwrath@users.noreply.github.com>
2020-02-27 01:34:14 -0500

    p2 buttons working on hello pop'n music

--- OpenParrot/src/Functions/Games/Other/Konami.cpp
+++ OpenParrot/src/Functions/Games/Other/Konami.cpp
@@ -8,23 +8,26 @@
 extern int* ffbOffset;
 static uint8_t STATUS_BUFFER[64]{};
 
-constexpr auto SERVICE  = (1 << 0);
-constexpr auto TEST     = (1 << 1);
-constexpr auto BUTTON1  = (1 << 2);
-constexpr auto BUTTON2  = (1 << 3);
-constexpr auto BUTTON3  = (1 << 4);
-constexpr auto BUTTON4  = (1 << 5);
-constexpr auto BUTTON5  = (1 << 6);
-constexpr auto BUTTON6  = (1 << 7);
-constexpr auto BUTTON7  = (1 << 8);
-constexpr auto BUTTON8  = (1 << 9);
-constexpr auto BUTTON9  = (1 << 10);
-constexpr auto BUTTON10 = (1 << 11);
+enum Buttons
+{
+    SERVICE = (1 << 0),
+    TEST = (1 << 1),
+    BUTTON1 = (1 << 2),
+    BUTTON2 = (1 << 3),
+    BUTTON3 = (1 << 4),
+    BUTTON4 = (1 << 5),
+    BUTTON5 = (1 << 6),
+    BUTTON6 = (1 << 7),
+    BUTTON7 = (1 << 8),
+    BUTTON8 = (1 << 9),
+    BUTTON9 = (1 << 10),
+    BUTTON10 = (1 << 11),
+};
 
 static void* __cdecl ac_io_hbhi_get_control_status_buffer(uint8_t* buffer)
 {
-	memcpy(buffer, STATUS_BUFFER, 64);
-	return buffer;
+    memcpy(buffer, STATUS_BUFFER, 64);
+    return buffer;
 }
 
 static char __cdecl ac_io_hbhi_update_control_status_buffer() 
@@ -77,8 +80,35 @@ static char __cdecl ac_io_hbhi_update_control_status_buffer()
             STATUS_BUFFER[12] |= 1 << 3;
         }
 
-        // TODO: player 2
-        STATUS_BUFFER[6] |= 1;
+        // player 2 start
+        if (!(buttons & BUTTON6))
+        {
+            STATUS_BUFFER[6] |= 1;
+        }
+
+        // player 1 red
+        if (buttons & BUTTON7)
+        {
+            STATUS_BUFFER[12] |= 1 << 4;
+        }
+
+        // player 1 blue
+        if (buttons & BUTTON8)
+        {
+            STATUS_BUFFER[12] |= 1 << 5;
+        }
+
+        // player 1 yellow
+        if (buttons & BUTTON9)
+        {
+            STATUS_BUFFER[12] |= 1 << 6;
+        }
+
+        // player 1 green
+        if (buttons & BUTTON10)
+        {
+            STATUS_BUFFER[12] |= 1 << 7;
+        }
 
         break;
     }
@@ -88,8 +118,8 @@ static char __cdecl ac_io_hbhi_update_control_status_buffer()
 
 DWORD WINAPI KonamiInput(LPVOID lpParam)
 {
-	// wait for SpiceTools to hook IO stuff.
-	Sleep(500);
+    // wait for SpiceTools to hook IO stuff.
+    Sleep(500);
     OutputDebugStringA("openparrot: attaching");
 
     // and replace it's IO hooks with ours >:)
@@ -111,5 +141,5 @@ DWORD WINAPI KonamiInput(LPVOID lpParam)
 
 static InitFunction KonamiFunc([]()
 {
-	CreateThread(NULL, 0, KonamiInput, NULL, 0, NULL); 
+    CreateThread(NULL, 0, KonamiInput, NULL, 0, NULL); 
 }, GameID::Konami);
