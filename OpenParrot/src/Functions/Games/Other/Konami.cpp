6e59c02b00c7b71a58f60850942f54a16401b3b0
nibs <Poliwrath@users.noreply.github.com>
2020-03-05 17:00:28 -0500

    cleanup konami more

--- OpenParrot/src/Functions/Games/Other/Konami.cpp
+++ OpenParrot/src/Functions/Games/Other/Konami.cpp
@@ -6,7 +6,8 @@
 
 // controls
 extern int* ffbOffset;
-static uint8_t STATUS_BUFFER[64]{};
+static uint8_t HBHI_BUFFER[64]{};
+static HMODULE game;
 
 enum Buttons
 {
@@ -26,13 +27,13 @@ enum Buttons
 
 static void* __cdecl ac_io_hbhi_get_control_status_buffer(uint8_t* buffer)
 {
-    memcpy(buffer, STATUS_BUFFER, 64);
+    memcpy(buffer, HBHI_BUFFER, 64);
     return buffer;
 }
 
 static char __cdecl ac_io_hbhi_update_control_status_buffer() 
 {
-    memset(STATUS_BUFFER, 0, 64);
+    memset(HBHI_BUFFER, 0, 64);
 
     int buttons = *ffbOffset;
 
@@ -42,72 +43,72 @@ static char __cdecl ac_io_hbhi_update_control_status_buffer()
 
         if (buttons & SERVICE)
         {
-            STATUS_BUFFER[5] |= 1 << 4;
+            HBHI_BUFFER[5] |= 1 << 4;
         }
 
         if (buttons & TEST)
         {
-            STATUS_BUFFER[5] |= 1 << 5;
+            HBHI_BUFFER[5] |= 1 << 5;
         }
 
         // player 1 start
         if (!(buttons & BUTTON1))
         {
-            STATUS_BUFFER[4] |= 1;
+            HBHI_BUFFER[4] |= 1;
         }
 
         // player 1 red
         if (buttons & BUTTON2)
         {
-            STATUS_BUFFER[12] |= 1 << 0;
+            HBHI_BUFFER[12] |= 1 << 0;
         }
 
         // player 1 blue
         if (buttons & BUTTON3)
         {
-            STATUS_BUFFER[12] |= 1 << 1;
+            HBHI_BUFFER[12] |= 1 << 1;
         }
 
         // player 1 yellow
         if (buttons & BUTTON4)
         {
-            STATUS_BUFFER[12] |= 1 << 2;
+            HBHI_BUFFER[12] |= 1 << 2;
         }
 
         // player 1 green
         if (buttons & BUTTON5)
         {
-            STATUS_BUFFER[12] |= 1 << 3;
+            HBHI_BUFFER[12] |= 1 << 3;
         }
 
         // player 2 start
         if (!(buttons & BUTTON6))
         {
-            STATUS_BUFFER[6] |= 1;
+            HBHI_BUFFER[6] |= 1;
         }
 
         // player 1 red
         if (buttons & BUTTON7)
         {
-            STATUS_BUFFER[12] |= 1 << 4;
+            HBHI_BUFFER[12] |= 1 << 4;
         }
 
         // player 1 blue
         if (buttons & BUTTON8)
         {
-            STATUS_BUFFER[12] |= 1 << 5;
+            HBHI_BUFFER[12] |= 1 << 5;
         }
 
         // player 1 yellow
         if (buttons & BUTTON9)
         {
-            STATUS_BUFFER[12] |= 1 << 6;
+            HBHI_BUFFER[12] |= 1 << 6;
         }
 
         // player 1 green
         if (buttons & BUTTON10)
         {
-            STATUS_BUFFER[12] |= 1 << 7;
+            HBHI_BUFFER[12] |= 1 << 7;
         }
 
         break;
@@ -115,54 +116,61 @@ static char __cdecl ac_io_hbhi_update_control_status_buffer()
 
         if (buttons & SERVICE)
         {
-            STATUS_BUFFER[5] |= 1 << 4;
+            HBHI_BUFFER[5] |= 1 << 4;
         }
 
         if (buttons & TEST)
         {
-            STATUS_BUFFER[5] |= 1 << 5;
+            HBHI_BUFFER[5] |= 1 << 5;
         }
 
         // view
         if (buttons & BUTTON1)
         {
-            STATUS_BUFFER[12] |= 1 << 2;
+            HBHI_BUFFER[12] |= 1 << 2;
         }
 
         // dimension toggle
         if (buttons & BUTTON2)
         {
-            STATUS_BUFFER[12] |= 1 << 3;
+            HBHI_BUFFER[12] |= 1 << 3;
         }
 
         // lever up
         if (buttons & BUTTON5)
         {
-            STATUS_BUFFER[12] |= 1 << 4;
+            HBHI_BUFFER[12] |= 1 << 4;
         }
 
         // lever down
         if (buttons & BUTTON4)
         {
-            STATUS_BUFFER[12] |= 1 << 5;
+            HBHI_BUFFER[12] |= 1 << 5;
         }
 
         // lever left
         if (buttons & BUTTON3)
         {
-            STATUS_BUFFER[12] |= 1 << 6;
+            HBHI_BUFFER[12] |= 1 << 6;
         }
 
         // lever right
         if (buttons & BUTTON6)
         {
-            STATUS_BUFFER[12] |= 1 << 7;
+            HBHI_BUFFER[12] |= 1 << 7;
         }
        
         break;
     }
 
     return 1;
+}
+
+void log(const char* msg)
+{
+    char buffer[512];
+    sprintf(buffer, "openparrot: %s", msg);
+    OutputDebugStringA(buffer);
 }
 
 DWORD WINAPI KonamiInput(LPVOID lpParam)
@@ -172,7 +180,7 @@ DWORD WINAPI KonamiInput(LPVOID lpParam)
     {
         for (int i = 0; i < 10; i++) 
         {
-            OutputDebugStringA("openparrot: unsupported game!");
+            log("unsupported game!");
         }
         Sleep(100);
         MessageBoxA(0, "Unsupported Konami Game!", "Error", MB_ICONERROR);
@@ -183,12 +191,12 @@ DWORD WINAPI KonamiInput(LPVOID lpParam)
 
     // wait for SpiceTools to hook IO stuff.
     Sleep(500);
-    OutputDebugStringA("openparrot: attaching");
+    log("attaching");
 
     // and replace it's IO hooks with ours >:)
     if (!LoadLibraryA("libacio.dll"))
     {
-        OutputDebugStringA("openparrot: failed to load libacio.dll?????");
+        log("failed to load libacio.dll?????");
         return 0;
     }
 
@@ -197,13 +205,19 @@ DWORD WINAPI KonamiInput(LPVOID lpParam)
     MH_CreateHookApi(L"libacio.dll", "ac_io_hbhi_update_control_status_buffer", ac_io_hbhi_update_control_status_buffer, NULL);
     MH_EnableHook(MH_ALL_HOOKS);
 
-    // TODO: Boomslangnz's patches
+    log("attached");
+
+    /*
+    // wait some more before patching
+    Sleep(250);
+
     if (ToBool(config["General"]["Always2D"]))
     {
-        OutputDebugStringA("openparrot: applying Always2D patch");
-    }
+        log("applying Always2D patch");
 
-    OutputDebugStringA("openparrot: attached");
+        log("applied Always2D patch");
+    }
+    */
 
     return 0;
 }
