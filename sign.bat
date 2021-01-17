692e25d4efe11af06753962a957f484d19f7e8d9
nzgamer41 <pslover9@gmail.com>
2021-01-18 00:39:58 +1300

    i really really really hate git (didnt push one of my changes)

--- sign.bat
+++ sign.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%cenc%"=="" goto end
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+
 IF %PLATFORM%==Win32 (
      C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool.exe sign /t http://timestamp.globalsign.com/scripts/timstamp.dll /f "cert" /p %cenc% /d .\build\bin\release\output\OpenParrot.dll .\build\bin\release\output\OpenParrot.dll
      C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool.exe sign /t http://timestamp.globalsign.com/scripts/timstamp.dll /f "cert" /p %cenc% /d .\build\bin\release\output\OpenParrotLoader.exe .\build\bin\release\output\OpenParrotLoader.exe
