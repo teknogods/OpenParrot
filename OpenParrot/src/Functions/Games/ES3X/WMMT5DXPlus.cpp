21639e01ca26c735d8595b8b913e82a71d82473a
Damon Murdoch <damonmurdoch1@gmail.com>
2022-04-18 18:00:10 +1000

    [04/18/2022 18:00:10] pre-test commit

--- OpenParrot/src/Functions/Games/ES3X/WMMT5DXPlus.cpp
+++ OpenParrot/src/Functions/Games/ES3X/WMMT5DXPlus.cpp
@@ -619,7 +619,7 @@ static int LoadGameData()
 			memcpy((void*)(storyOffset), saveDatadxp + 0xE0, 0x16);
 
 			// Dump the data that is being copied to a file
-			writeDump("openprogress_test.sav", saveDatadxp + 0xE0, 0x16);
+			writeDump("openprogress_test.sav", (unsigned char *)(saveDatadxp + 0xE0), 0x16);
 
 			// First page
 			//memcpy((void *)(storyOffset), saveDatadxp, 0x08);
