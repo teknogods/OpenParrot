03bbba2c157db94519a0de48904e60c2c2f8ff2e
Damon Murdoch <damonmurdoch1@gmail.com>
2022-04-22 01:35:44 +1000

    [04/22/2022 01:35:44] pre-test commit

--- OpenParrot/src/Functions/Games/ES3X/WMMT5DXPlus.cpp
+++ OpenParrot/src/Functions/Games/ES3X/WMMT5DXPlus.cpp
@@ -554,10 +554,10 @@ static int loadCarFile(char* filename)
 			memcpy((void*)(carSaveLocation + 0x3C), carDataDxp + 0x3C, 4); // CustomColor (0x3C)
 			memcpy((void*)(carSaveLocation + 0x40), carDataDxp + 0x40, 8); // Rims (0x40), Rims Colour (0x44)
 			memcpy((void*)(carSaveLocation + 0x48), carDataDxp + 0x48, 8); // Aero (0x48), Hood (0x4C)
-			// memcpy((void*)(carSaveLocation + 0x50), carDataDxp + 0x50, 8); // ??
+			memcpy((void*)(carSaveLocation + 0x50), carDataDxp + 0x50, 8); // ??
 			memcpy((void*)(carSaveLocation + 0x58), carDataDxp + 0x58, 8); // Wing (0x58), Mirror (0x5C)
 			memcpy((void*)(carSaveLocation + 0x60), carDataDxp + 0x60, 8); // Sticker (0x60), Sticker Type (0x64)
-			memcpy((void*)(carSaveLocation + 0x68), carDataDxp + 0x60, 8); // ??
+			// memcpy((void*)(carSaveLocation + 0x68), carDataDxp + 0x60, 8); // ??
 			memcpy((void*)(carSaveLocation + 0x88), carDataDxp + 0x88, 8); // Roof Sticker (0x88), Roof Sticker Type (0x8C)
 			memcpy((void*)(carSaveLocation + 0x90), carDataDxp + 0x90, 8); // Neon (0x90), Trunk (0x94)
 			memcpy((void*)(carSaveLocation + 0x98), carDataDxp + 0x98, 8); // Plate Frame (0x98)
