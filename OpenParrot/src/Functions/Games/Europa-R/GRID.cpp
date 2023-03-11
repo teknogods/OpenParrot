50390eef3c480d90ca41d3caf53c0632ada350a5
Boomslangnz <Boomslangnz@hotmail.com>
2023-03-11 15:49:10 +1300

    GRID - New Inputs on Buttons etc

--- OpenParrot/src/Functions/Games/Europa-R/GRID.cpp
+++ OpenParrot/src/Functions/Games/Europa-R/GRID.cpp
@@ -58,14 +58,6 @@ float toFloatGRID(uint8_t x)
 	return x / 255.0;
 }
 
-void SetButton(DWORD address, bool pressed)
-{
-	if (pressed)
-		*(DWORD*)address = 0x3F800000;
-	else
-		*(DWORD*)address = 0;
-}
-
 DWORD WINAPI XInputGetStateGRID
 (
 	__in  DWORD         dwUserIndex,						// Index of the gamer associated with the device
@@ -81,8 +73,6 @@ DWORD WINAPI XInputGetStateGRID
 			inputInitialized = true;
 		}
 
-		DWORD buttons2 = *wheelSection;
-		DWORD buttons = *ffbOffset;
 		BYTE wheel = *ffbOffset2;
 		BYTE gas = *ffbOffset3;
 		BYTE brake = *ffbOffset4;
@@ -100,18 +90,8 @@ DWORD WINAPI XInputGetStateGRID
 		// Brake Float
 		memcpy((void *)(myPointer + 0x494), &brakeFloat, 4);
 
-		SetButton(myPointer + 0x4, buttons & 0x1);
-		SetButton(myPointer + 0x1C, buttons & 0x2);
-		SetButton(myPointer + 0x24, buttons & 0x4);
-
-		SetButton(myPointer + 0xC, buttons2 & 0x1);
-		SetButton(myPointer + 0x14, buttons2 & 0x2);
-		SetButton(myPointer + 0x4C, buttons2 & 0x4);
-		SetButton(myPointer + 0x414, buttons2 & 0x8);
-		SetButton(myPointer + 0x404, buttons2 & 0x10);
-		SetButton(myPointer + 0x3C, buttons2 & 0x20);
-		SetButton(myPointer + 0x34, buttons2 & 0x40);
-		SetButton(myPointer + 0x40c, buttons2 & 0x80);
+		// FFB Wheel Float
+		memcpy((void*)(myPointer + 0x4), &wheelFloat, 4);
 	}
 	if (GameDetect::currentGame == GameID::GRID)
 	{
@@ -122,6 +102,169 @@ DWORD WINAPI XInputGetStateGRID
 		return ERROR_DEVICE_NOT_CONNECTED;
 	}
 }
+
+static bool InputsInit;
+static int InputsCount;
+
+static DWORD StartButton;
+static DWORD ViewButton;
+static DWORD ShiftUpButton;
+static DWORD ShiftDownButton;
+static DWORD ResetButton;
+static DWORD LookBehindButton;
+static DWORD LookBehind2Button;
+static DWORD PauseButton;
+static DWORD ReplayButton;
+static DWORD LookLeftButton;
+static DWORD LookRightButton;
+static DWORD SelectButton;
+static DWORD Gear1Button;
+static DWORD Gear2Button;
+static DWORD Gear3Button;
+static DWORD Gear4Button;
+static DWORD Gear5Button;
+static DWORD Gear6Button;
+static DWORD GearReverseButton;
+
+static int(__fastcall* ButtonInputsOri)(int a1, __int64 a2);
+static int __fastcall ButtonInputsHook(int a1, __int64 a2)
+{
+	ButtonInputsOri(a1, a2);
+
+	unsigned int ValueRead{};
+	int Count{};
+	int InputsAddress{};
+
+	if (!InputsInit)
+	{
+		++InputsCount;
+
+		int BaseRead = *(DWORD*)(a1 + 68);
+
+		if (BaseRead)
+			ValueRead = (*(DWORD*)(a1 + 72) - BaseRead) >> 2;
+		else
+			ValueRead = 0;
+
+		if (ValueRead)
+		{
+			InputsAddress = *(DWORD*)(*(DWORD*)(a1 + 68) + 4 * Count);
+			++Count;
+		}
+
+		switch (InputsCount)
+		{
+		case 1:
+			PauseButton = InputsAddress + 16;
+			break;
+		case 3:
+			ResetButton = InputsAddress + 16;
+			break;
+		case 18:
+			ViewButton = InputsAddress + 16;
+			break;
+		case 19:
+			ShiftUpButton = InputsAddress + 16;
+			break;
+		case 20:
+			ShiftDownButton = InputsAddress + 16;
+			break;
+		case 21:
+			Gear1Button = InputsAddress + 16;
+			break;
+		case 22:
+			Gear2Button = InputsAddress + 16;
+			break;
+		case 23:
+			Gear3Button = InputsAddress + 16;
+			break;
+		case 24:
+			Gear4Button = InputsAddress + 16;
+			break;
+		case 25:
+			Gear5Button = InputsAddress + 16;
+			break;
+		case 26:
+			Gear6Button = InputsAddress + 16;
+			break;
+		case 27:
+			GearReverseButton = InputsAddress + 16;
+			break;
+		case 28:
+			LookLeftButton = InputsAddress + 16;
+			break;
+		case 29:
+			LookRightButton = InputsAddress + 16;
+			break;
+		case 31:
+			LookBehind2Button = InputsAddress + 16;
+			break;
+		case 32:
+			LookBehindButton = InputsAddress + 16;
+			break;
+		case 33:
+			ResetButton = InputsAddress + 16;
+			break;
+		case 38:
+			SelectButton = InputsAddress + 16;
+			break;
+		case 39:
+			StartButton = InputsAddress + 16;
+			break;
+		case 46:
+			InputsInit = true;
+			break;
+		}
+	}
+
+	if (StartButton)
+		*ffbOffset & 0x01 ? *(DWORD*)(StartButton) = 0x01 : *(DWORD*)(StartButton) = 0x00;
+	if (ShiftUpButton)
+		*ffbOffset & 0x02 ? *(DWORD*)(ShiftUpButton) = 0x01 : *(DWORD*)(ShiftUpButton) = 0x00;
+	if (ShiftDownButton)
+		*ffbOffset & 0x04 ? *(DWORD*)(ShiftDownButton) = 0x01 : *(DWORD*)(ShiftDownButton) = 0x00;
+	if (ViewButton)
+		*wheelSection & 0x01 ? *(DWORD*)(ViewButton) = 0x01 : *(DWORD*)(ViewButton) = 0x00;
+	if (ResetButton)
+		*wheelSection & 0x02 ? *(DWORD*)(ResetButton) = 0x01 : *(DWORD*)(ResetButton) = 0x00;
+	if (LookBehindButton)
+		*wheelSection & 0x04 ? *(DWORD*)(LookBehindButton) = 0x01 : *(DWORD*)(LookBehindButton) = 0x00;
+	if (LookLeftButton)
+		*wheelSection & 0x08 ? *(DWORD*)(LookLeftButton) = 0x01 : *(DWORD*)(LookLeftButton) = 0x00;
+	if (LookRightButton)
+		*wheelSection & 0x10 ? *(DWORD*)(LookRightButton) = 0x01 : *(DWORD*)(LookRightButton) = 0x00;
+	if (PauseButton)
+		*wheelSection & 0x20 ? *(DWORD*)(PauseButton) = 0x01 : *(DWORD*)(PauseButton) = 0x00;
+	if (ReplayButton)
+		*wheelSection & 0x40 ? *(DWORD*)(ReplayButton) = 0x01 : *(DWORD*)(ReplayButton) = 0x00;
+	if (LookBehind2Button)
+		*wheelSection & 0x80 ? *(DWORD*)(LookBehind2Button) = 0x01 : *(DWORD*)(LookBehind2Button) = 0x00;
+	if (Gear1Button)
+		*wheelSection & 0x100 ? *(DWORD*)(Gear1Button) = 0x01 : *(DWORD*)(Gear1Button) = 0x00;
+	if (Gear2Button)
+		*wheelSection & 0x200 ? *(DWORD*)(Gear2Button) = 0x01 : *(DWORD*)(Gear2Button) = 0x00;
+	if (Gear3Button)
+		*wheelSection & 0x400 ? *(DWORD*)(Gear3Button) = 0x01 : *(DWORD*)(Gear3Button) = 0x00;
+	if (Gear4Button)
+		*wheelSection & 0x800 ? *(DWORD*)(Gear4Button) = 0x01 : *(DWORD*)(Gear4Button) = 0x00;
+	if (Gear5Button)
+		*wheelSection & 0x1000 ? *(DWORD*)(Gear5Button) = 0x01 : *(DWORD*)(Gear5Button) = 0x00;
+	if (Gear6Button)
+		*wheelSection & 0x2000 ? *(DWORD*)(Gear6Button) = 0x01 : *(DWORD*)(Gear6Button) = 0x00;
+	if (GearReverseButton)
+		*wheelSection & 0x4000 ? *(DWORD*)(GearReverseButton) = 0x01 : *(DWORD*)(GearReverseButton) = 0x00;
+
+	if (SelectButton)
+	{
+		if (*ffbOffset3 > 0x10)
+			*(DWORD*)(SelectButton) = 0x01;
+		else
+			*(DWORD*)(SelectButton) = 0x00;
+	}
+
+	return 0;
+}
+
 
 static InitFunction GRIDFunc([]()
 {
@@ -145,8 +288,11 @@ static InitFunction GRIDFunc([]()
 	// Force button mashing on
 	injector::MakeNOP(0x00B76560, 6);
 
-	// Hook XInput to send our controls
+	// Button Inputs
+	injector::MakeNOP(0xB8CECD, 3);
+
 	MH_Initialize();
+	MH_CreateHook((void*)0xB8CDD0, ButtonInputsHook, (void**)&ButtonInputsOri);
 	MH_CreateHookApi(L"xinput1_3.dll", "XInputGetState", &XInputGetStateGRID, NULL);
 	MH_EnableHook(MH_ALL_HOOKS);
 
