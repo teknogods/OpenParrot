72295f82776efde26054f1024a41d62ce2932df5
Emi Midnight <emi@bemani.gay>
2023-11-04 02:01:01 +0100

    - WMMT5DX: stop it from messing with system time

--- OpenParrot/src/Functions/Games/ES3X/WMMT5DX.cpp
+++ OpenParrot/src/Functions/Games/ES3X/WMMT5DX.cpp
@@ -850,6 +850,15 @@ static DWORD WINAPI Wmmt5FfbCollector(void* ctx)
 	}
 }
 
+//stop the game from changing the time...
+typedef bool (WINAPI* SETSYSTEMTIME)(SYSTEMTIME* in);
+SETSYSTEMTIME pSetSystemTime = NULL;
+
+bool WINAPI Hook_SetSystemTime(SYSTEMTIME* in)
+{
+	return TRUE;
+}
+
 static InitFunction Wmmt5DXFunc([]()
 {
 	// folder for path redirections
@@ -894,7 +903,8 @@ static InitFunction Wmmt5DXFunc([]()
 	MH_CreateHookApi(L"hasp_windows_x64_98199.dll", "hasp_login", Hook_hasp_login, NULL);
 	MH_CreateHookApi(L"WS2_32", "bind", Hook_bind, reinterpret_cast<LPVOID*>(&pbind));
 
-
+	// needed otherwise it will change the system wide date and time
+	MH_CreateHookApi(L"KERNEL32", "SetSystemTime", Hook_SetSystemTime, reinterpret_cast<LPVOID*>(&pSetSystemTime));
 
 	GenerateDongleData(isTerminal);
 
